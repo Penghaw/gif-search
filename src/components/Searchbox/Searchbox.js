dc3f4cd2ca8acd95d0f6047adefb4f4dca9ef0b1
Rivaldi Penghaw Logan <rivaldi.logan@hotmail.com>
2019-08-10 19:44:50 +0700

    Added component - Searchbox

---
+++ src/components/Searchbox/Searchbox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Searchbox.css';
+
+class Searchbox extends React.Component {
+    render() {
+        return (
+            <div>
+                <input className="searchGif"/>
+            </div>
+        );
+    }
+}
+
+export default Searchbox;
