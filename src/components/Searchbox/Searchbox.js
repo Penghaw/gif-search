d1a25ae1b8d7a7c919dad6002b4d712257740d5e
Rivaldi Penghaw Logan <rivaldi.logan@hotmail.com>
2019-08-10 20:14:01 +0700

    Added listener to searchbox

--- src/components/Searchbox/Searchbox.js
+++ src/components/Searchbox/Searchbox.js
@@ -2,10 +2,35 @@ import React from 'react';
 import './Searchbox.css';
 
 class Searchbox extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            query: '',
+            typingTimeout: false
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange = (e) => {
+        let query = e.target.value;
+        if (this.state.typingTimeout) clearTimeout(this.state.typingTimeout);
+
+        const self = this;
+        this.setState({
+            typingTimeout: setTimeout(function () {
+                self.setState({
+                    query: query,
+                    typingTimeout: false
+                });
+            }, 500)
+        });
+    };
+
     render() {
         return (
             <div>
-                <input className="searchGif"/>
+                <input className="searchGif" onChange={this.onInputChange}/>
             </div>
         );
     }
