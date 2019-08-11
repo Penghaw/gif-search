d91a83d31a42c53dd6c46eea13c6f2ba6319dc7b
Rivaldi Penghaw Logan <rivaldi.logan@hotmail.com>
2019-08-11 10:10:52 +0700

    LoadingSpinner now accepts property isLoading and renders conditionally

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './LoadingSpinner.css';
 
-const LoadingSpinner = () => {
-    return (
-        <div className="sk-folding-cube">
-            <div className="sk-cube1 sk-cube"></div>
-            <div className="sk-cube2 sk-cube"></div>
-            <div className="sk-cube4 sk-cube"></div>
-            <div className="sk-cube3 sk-cube"></div>
-        </div>
-    )
+const LoadingSpinner = (props) => {
+    const isLoading = props.isLoading;
+    if (isLoading) {
+        return (
+            <div className="sk-folding-cube">
+                <div className="sk-cube1 sk-cube"></div>
+                <div className="sk-cube2 sk-cube"></div>
+                <div className="sk-cube4 sk-cube"></div>
+                <div className="sk-cube3 sk-cube"></div>
+            </div>
+        )
+    } else {
+        return null;
+    }
 };
 
 export default LoadingSpinner;
